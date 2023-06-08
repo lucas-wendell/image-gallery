dbfd40c3c9c723955d697d286ba47c27bf9248df
Lucas Wendell <lucaswhendellf@gmail.com>
2023-06-08 17:05:52 -0300

    chore: config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,32 @@
+/* eslint-disable no-undef */
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	plugins: ['react-refresh', 'import', '@typescript-eslint', 'autofix'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'arrow-body-style': ['error', 'as-needed'],
+		'@typescript-eslint/consistent-type-imports': [
+			'error',
+			{
+				prefer: 'type-imports',
+			},
+		],
+		'no-restricted-imports': [
+			'error',
+			{
+				patterns: ['../'],
+			},
+		],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
+};
