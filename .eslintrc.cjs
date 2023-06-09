0d5c4f20d3c584ee64ac1378caa4124be4d68c3a
Lucas Wendell <lucaswhendellf@gmail.com>
2023-06-09 14:21:01 -0300

    feat: change some stuffs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,31 +2,26 @@
 module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:import/errors',
-		'plugin:import/warnings',
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:import/errors",
+		"plugin:import/warnings",
 	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	plugins: ['react-refresh', 'import', '@typescript-eslint', 'autofix'],
+	parser: "@typescript-eslint/parser",
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	plugins: ["react-refresh", "import", "@typescript-eslint", "autofix"],
 	rules: {
-		'react-refresh/only-export-components': 'warn',
-		'arrow-body-style': ['error', 'as-needed'],
-		'@typescript-eslint/consistent-type-imports': [
-			'error',
+		"react-refresh/only-export-components": "warn",
+		"arrow-body-style": ["error", "as-needed"],
+		"@typescript-eslint/consistent-type-imports": [
+			"error",
 			{
-				prefer: 'type-imports',
+				prefer: "type-imports",
 			},
 		],
-		'no-restricted-imports': [
-			'error',
-			{
-				patterns: ['../'],
-			},
-		],
-		'react-hooks/rules-of-hooks': 'error',
-		'react-hooks/exhaustive-deps': 'warn',
+
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
 	},
 };
--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,16 +1,10 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline bg-sky-500 hover:bg-sky-700">
-				Hello world!
-			</h1>
-			<button className="dark:md:hover:bg-fuchsia-600">Save changes</button>
-			<FontAwesomeIcon icon={faUser} />
-		</>
-	);
-}
+const App: React.FC = () => (
+	<>
+		<h1 className="text-3xl font-bold underline bg-sky-500 hover:bg-sky-700">
+			Hello world!
+		</h1>
+		<button className="dark:md:hover:bg-fuchsia-600">Save changes</button>
+	</>
+);
 
 export default App;
