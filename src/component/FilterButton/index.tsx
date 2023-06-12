0f6958bd81c6c5a12e90e0c0c29d9b0b0c3186ac
Lucas Wendell <lucaswhendellf@gmail.com>
2023-06-12 15:04:12 -0300

    style: finish filter button style

--- src/component/FilterButton/index.tsx
+++ src/component/FilterButton/index.tsx
@@ -2,14 +2,28 @@ import React from "react";
 
 type FilterButtonProps = {
 	name: string;
+	type?: "primary" | "secondary";
 };
 
 const FilterButton: React.FC<FilterButtonProps> = ({
 	name,
-}: FilterButtonProps) => (
-	<a className=" bg-transparent-bg text-dark-blue py-2.5 px-3 rounded-lg whitespace-nowrap">
-		{name}
-	</a>
-);
+	type = "primary",
+}: FilterButtonProps) => {
+	const styles = {
+		primary:
+			"text-main-gray hover:text-dark-blue transition-all duration-300 ease-in-out",
+		secondary: "bg-transparent-bg text-dark-blue rounded-lg font-bold",
+	};
+	return (
+		<a
+			className={`py-2.5 px-3 whitespace-nowrap capitalize cursor-pointer ${styles[type]}`}
+			type="button"
+			title={name}
+			aria-label={name}
+		>
+			{name}
+		</a>
+	);
+};
 
 export default FilterButton;
--- src/component/FiltersNavBar/index.tsx
+++ src/component/FiltersNavBar/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import FilterButton from "../FilterButton";
 
 const FiltersNavBar: React.FC = () => (
-	<nav className="flex gap-2.5 overflow-x-auto w-4/5">
-		<FilterButton name={"lucas wendell"} />
+	<nav className="flex gap-2.5 overflow-x-auto w-4/5 scrollbar-thin scrollbar-thumb-pink">
+		<FilterButton name={"lucas wendell"} type="secondary" />
 		<FilterButton name={"lucas wendell"} />
 		<FilterButton name={"lucas wendell"} />
 		<FilterButton name={"lucas wendell"} />
@@ -12,5 +12,4 @@ const FiltersNavBar: React.FC = () => (
 		<FilterButton name={"lucas wendell"} />
 	</nav>
 );
-
 export default FiltersNavBar;
