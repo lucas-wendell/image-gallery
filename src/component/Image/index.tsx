2d0b25588cf49f27f059cb5fb59b1f1bc74d87ea
Lucas Wendell <lucaswhendellf@gmail.com>
2023-06-20 16:38:39 -0300

    feat: create feature to add default icon on profilte user image throw a error

--- src/component/Image/index.tsx
+++ src/component/Image/index.tsx
@@ -1,6 +1,6 @@
-import { faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faHeart, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
 type ImageProps = {
 	views: number;
@@ -16,39 +16,54 @@ const Image: React.FC<ImageProps> = ({
 	userImageURL,
 	searchPictureURL,
 	userName,
-}: ImageProps) => (
-	<div className="space-y-4 h-min">
-		<div
-			className="w-full h-56 bg-cover bg-center bg-no-repeat rounded-lg"
-			style={{ backgroundImage: `url(${searchPictureURL})` }}
-		/>
-		<div className="flex items-center justify-between">
-			<div className="flex items-center gap-2">
-				<img
-					src={userImageURL}
-					alt="user picture"
-					className="w-8 h-8 rounded-full"
-				/>
-				<p>{userName}</p>
-			</div>
-			<div className="flex items-center gap-2">
-				<div className="space-x-1 text-gray">
-					<FontAwesomeIcon
-						icon={faHeart}
-						className="cursor-pointer hover:text-pink transition-all duration-300 ease-in-out"
-					/>
-					<span className="text-main-gray font-medium">{likes}</span>
+}: ImageProps) => {
+	const [imageError, setImageError] = useState(false);
+
+	const handleImageError = () => {
+		setImageError(() => true);
+	};
+
+	return (
+		<div className="space-y-4 h-min">
+			<div
+				className="w-full h-56 bg-cover bg-center bg-no-repeat rounded-lg"
+				style={{ backgroundImage: `url(${searchPictureURL})` }}
+			/>
+			<div className="flex items-center justify-between">
+				<div className="flex items-center gap-2">
+					{imageError ? (
+						<div className="w-7 h-7 border border-pink flex items-center justify-center rounded-full">
+							<FontAwesomeIcon icon={faUser} className="w-4 h-4 text-pink" />
+						</div>
+					) : (
+						<img
+							src={userImageURL}
+							alt="user picture"
+							className="w-8 h-8 rounded-full"
+							onError={handleImageError}
+						/>
+					)}
+					<p>{userName}</p>
 				</div>
-				<div className="space-x-1 text-gray">
-					<FontAwesomeIcon
-						icon={faEye}
-						className="cursor-pointer hover:text-pink transition-all duration-300 ease-in-out"
-					/>
-					<span className="text-main-gray font-medium">{views}</span>
+				<div className="flex items-center gap-2">
+					<div className="space-x-1 text-gray">
+						<FontAwesomeIcon
+							icon={faHeart}
+							className="cursor-pointer hover:text-pink transition-all duration-300 ease-in-out"
+						/>
+						<span className="text-main-gray font-medium">{likes}</span>
+					</div>
+					<div className="space-x-1 text-gray">
+						<FontAwesomeIcon
+							icon={faEye}
+							className="cursor-pointer hover:text-pink transition-all duration-300 ease-in-out"
+						/>
+						<span className="text-main-gray font-medium">{views}</span>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Image;
